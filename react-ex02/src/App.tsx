52da09d4d676629777b798cae5a1916ef462df89
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-03-26 22:00:18 -0300

    Atualização do projeto

--- react-ex02/src/App.tsx
+++ react-ex02/src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import { useState } from 'react';
+import styles from './App.module.css';
+import poweredImage from './assets/powered.png'
 
-function App() {
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weigthField, setWeigthField] = useState<number>(0);
+
+  const handleCalculateButton = () => {
+    if(heightField && weigthField) {
+
+    } else {
+      alert("Preencha todos os campos.");
+    }
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={150} />
+        </div>
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC.</h1>
+          <p>IMC é a sigla para o Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
+
+          <input 
+            type="number"
+            placeholder="Digite a sua altura. Ex: 1.5 (em metros)"
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+          <input 
+            type="number"
+            placeholder="Digite o seu peso. Ex: 75.3 (em kg)"
+            value={weigthField > 0 ? weigthField : ''}
+            onChange={e => setWeigthField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
+    </div>
   );
 }
 
