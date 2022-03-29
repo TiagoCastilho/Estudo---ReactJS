c9c1022db5f697b67179cc7ba10c508540cbf87a
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-03-29 18:56:33 -0300

    Projeto requisição GET simples

--- react-ex03/src/App.tsx
+++ react-ex03/src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Movie } from "./types/Movie";
 
-function App() {
+const App = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const loadMovies = () => {
+    fetch('https://api.b7web.com.br/cinema/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setMovies(json);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="block bg-blue-400 p-2 rounded" onClick={loadMovies}>Carregar Filmes</button>
+
+      Total de Filmes: {movies.length}
+
+      <div className="grid grid-cols-6 gap-3">
+        {movies.map((item, index) => (
+          <div>
+            <img src={item.avatar} alt="" className="w-32 block" />
+            {item.titulo}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
