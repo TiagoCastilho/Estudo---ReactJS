fc9d0fa580223968fe451cd51c95f3eace93c1e0
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-02 15:06:53 -0300

    Inclusão do Projeto 06 - Jogo da Memória

---
+++ react-ex06/src/App.tsx
@@ -0,0 +1,34 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+import RestartIcon from './svgs/restart.svg';
+import { Butoon } from './components/Button';
+import { InfoItem } from './components/InfoItem/Index';
+
+const App = () => {
+
+  const resetAndCreateGrid = () => {
+
+  }
+
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} width='200' alt="" />
+        </C.LogoLink>
+
+        <C.InfoArea>
+          <InfoItem label='Tempo' value='00:00' />
+          <InfoItem label='Movimentos' value='0' />
+        </C.InfoArea>
+
+        <Butoon label='Reiniciar' icon={RestartIcon} onClick={resetAndCreateGrid}/>
+      </C.Info>
+      <C.GridArea>
+        ...
+      </C.GridArea>
+    </C.Container>
+  );
+}
+
+export default App;
