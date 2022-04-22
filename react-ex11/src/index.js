0bcaf9f904e8cc1c3b7b0c40b90bf77aab4c0bdc
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-22 20:24:31 -0300

    Criando estrutura e instalando bibliotecas

--- react-ex11/src/index.js
+++ react-ex11/src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { configureStore } from '@reduxjs/toolkit';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './App';
 import Reducers from './Reducers';
 
-const store = configureStore(Reducers);
+const store = createStore(Reducers);
+
 
 ReactDOM.render(
   <Provider store={store}>
       <App />
   </Provider>
 , document.getElementById('root'));
-
-/*
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-);
-*/
--- react-ex11/src/reducers/userReducer.js
+++ react-ex11/src/reducers/userReducer.js
@@ -1,21 +1,6 @@
-/*
-const initialState = {  
-    email: ''
-};
-
-export default (state = initialState, action) => {
-
-    if(action.type === 'SET_EMAIL') {
-        return { ...state, email:action.payload.email };
-    }
-
-    return state;
-}
-*/
-
 const initialState = {  
     email: '',
-}
+};
 
 const useReducer = ( state = initialState, action ) => {
 
