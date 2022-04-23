70b2622769e22a2fcf679b3647ff4e9392aa9706
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-23 13:47:13 -0300

    Página 404

--- react-ex11/src/Routes.js
+++ react-ex11/src/Routes.js
@@ -3,12 +3,15 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 export default () => {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound />} />
+
         </Routes>
     );
 }
