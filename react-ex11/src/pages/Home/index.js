4952aebfae5b2b19c8507bb5d8f9932aa9e76095
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-24 11:48:52 -0300

    Home (1 - searchBox)

--- react-ex11/src/pages/Home/index.js
+++ react-ex11/src/pages/Home/index.js
@@ -1,14 +1,37 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { SearchArea, SignInArea } from './styled';
+import useApi from '../../helpers/HelperAPI';
 
-const Home = () => {
+import { PageContainer } from "../../components/MainComponents";
+
+const SignIn = () => {
+    const api = useApi();
+
     return (
-        <div>
-            <h1>Home</h1>
-            
-            <Link to="/About">Sobre</Link>
-        </div>
+        <>
+            <SearchArea>
+                <PageContainer>
+                    <div className="searchBox">
+                        <form method="GET" action="/ads">
+                            <input type='text' name='q' placeholder="O que você procura?" />
+                            <select name="state">
+
+                            </select>
+                            <button>Pesquisar</button>
+                        </form>
+                    </div>
+                    <div className="categoryList">
+
+                    </div>
+                </PageContainer>
+            </SearchArea>
+            <PageContainer>
+                <SignInArea>                
+                    ...
+                </SignInArea>
+            </PageContainer>
+        </>
     );
 }
 
-export default Home;
+export default SignIn;
