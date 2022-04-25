030b14d1e14940e8c14e4070f495e2b9812cd4f2
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-25 16:41:41 -0300

    AddAd (1 - estrutura)

--- react-ex11/src/Routes.js
+++ react-ex11/src/Routes.js
@@ -8,6 +8,7 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import AdPage from './pages/AdPage';
+import AddAd from './pages/AddAd';
 import NotFound from './pages/NotFound';
 
 export default () => {
@@ -24,7 +25,7 @@ export default () => {
             <Route path="/ad/:id" element={<AdPage />} />
             <Route path="/post-an-ad" element={
                 <RequireAuth>
-                    <About />
+                    <AddAd />
                 </RequireAuth>    
             } />
             <Route path="*" element={<NotFound />} />
