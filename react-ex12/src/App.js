5b8db094df03e1e491144e227724909e4634f4bb
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-27 15:43:29 -0300

    Estrutura

--- react-ex12/src/App.js
+++ react-ex12/src/App.js
@@ -2,24 +2,39 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { useSelector } from 'react-redux';
 
+import { Container, Menu, PageBody } from './AppStyled';
+
 import HomeScreen from './pages/HomeScreen';
 import Tela2Screen from './pages/Tela2Screen';
+
+import store from './assets/store.png';
+import order from './assets/order.png';
+import profile from './assets/profile.png';
+
+import MenuItem from './components/MenuItem';
 
 export default () => {
     const name = useSelector(state => state.user.name);
 
     return (
         <BrowserRouter>
-            <h1>Oi, {name}</h1>
-
-            <Switch>
-                <Route exact path="/">
-                    <HomeScreen />
-                </Route>
-                <Route path="/tela2/:nome">
-                    <Tela2Screen />
-                </Route>
-            </Switch>
+            <Container>
+                <Menu>
+                    <MenuItem icon={store} link="/" />
+                    <MenuItem icon={order} link="/orders" />
+                    <MenuItem icon={profile} link="/profile" />
+                </Menu>
+                <PageBody>
+                    <Switch>
+                        <Route exact path="/">
+                            <HomeScreen />
+                        </Route>
+                        <Route path="/tela2/:nome">
+                            <Tela2Screen />
+                        </Route>
+                    </Switch>
+                </PageBody>
+            </Container>
         </BrowserRouter>
     );
 }
