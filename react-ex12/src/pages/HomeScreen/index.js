354df4f3e591f4eeed9fb070cb8b3993ee1dcd7d
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-04-28 17:08:25 -0300

    Estruturação da API

--- react-ex12/src/pages/HomeScreen/index.js
+++ react-ex12/src/pages/HomeScreen/index.js
@@ -1,12 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from "react-router-dom";
 import { Container} from './styled';
+
+import api from '../../api';
 
 import Header from '../../components/Header';
 
 export default () => {
     const history = useHistory();
     const [ headerSearch, setHeaderSearch ] = useState('');
+
+    useEffect(() => {
+        const getCategories = async () => {
+            const categories = await api.categories();
+        };
+
+        getCategories();
+    }, []);
 
     return (
         <Container>
