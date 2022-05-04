d4942f2bab9f9d08d3f482bd0d73817e7106af07
Tiago Castilho <tiagocastilho_bm@hotmail.com>
2022-05-03 21:47:25 -0300

    Ads (2 - leftSide)

--- react-ex11/src/pages/Ads/index.js
+++ react-ex11/src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { PageArea } from './styled';
 import useApi from '../../helpers/HelperAPI';
 
@@ -8,6 +8,15 @@ import AdItem from "../../components/partials/AdItem";
 
 const SignIn = () => {
     const api = useApi();
+
+    const useQueryString = () => {
+        return new URLSearchParams( useLocation().search );
+    }
+    const query = useQueryString();
+
+    const [q, setQ] = useState( query.get('q') != null ? query.get('q') : '' );
+    const [cat, setCat] = useState( query.get('cat') != null ? query.get('cat') : '' );
+    const [state, setState] = useState( query.get('state') != null ? query.get('state') : '' );
 
     const [stateList, setStateList] = useState([]);
     const [categories, setCategories] = useState([]);
@@ -45,10 +54,15 @@ const SignIn = () => {
             <PageArea>
                 <div className="leftSide">
                     <form method="GET">
-                        <input type="text" name="q" />
+                        <input 
+                            type="text" 
+                            name="q" 
+                            placeholder="O que você procura?"
+                            value={q}
+                        />
 
                         <div className="filterName">Estado:</div>
-                        <select name="state">
+                        <select name="state" value={state}>
                             <option></option>
                             {stateList.map((i, k)=>
                                 <option key={k} value={i.name}>{i.name}</option>
@@ -58,7 +72,7 @@ const SignIn = () => {
                         <div className="filterName">Categoria:</div>
                         <ul>
                             {categories.map((i, k)=>
-                                <li key={k} className="categoryItem">
+                                <li key={k} className={cat==i.slug?'categoryItem active':'categoryItem'}>
                                     <img src={i.img} alt="" />
                                     <span>{i.name}</span>
                                 </li>
